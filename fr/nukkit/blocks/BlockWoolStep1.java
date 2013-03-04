336e2d85f36ffe1a84840b52cbabae7aaeaaca5d
Mathieu Bour <dierka@live.fr>
2013-03-04 23:29:43 +0100

    Fixed a texture bug with the wool slabs.

--- fr/nukkit/blocks/BlockWoolStep1.java
+++ fr/nukkit/blocks/BlockWoolStep1.java
@@ -11,95 +11,69 @@ import net.minecraft.src.Material;
 
 public class BlockWoolStep1 extends BlockHalfSlab
 {
-    public static final String[] dyeColorNames = new String[] {"black", "red", "green", "brown", "blue", "purple", "cyan", "silver" };
-    
+    public static final String[] woolType = new String[] { "white", "orange", "magenta","lightBlue", "yellow", "lime", "pink", "gray" };
+
     public BlockWoolStep1(int par1, boolean par2)
     {
-        super(par1, par2, Material.rock);
+        super(par1, par2, Material.wood);
         this.setCreativeTab(CreativeTabs.tabBlock);
     }
-
-    /**
-     * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
-     */
+    
     public int getBlockTextureFromSideAndMetadata(int par1, int par2)
     {
-        int metadata = par2 & 7;
-        
-        switch(metadata)
+        switch (par2 & 7)
         {
-	    	case 0:
-	    		return 64;
 	    	case 1:
 	    		return 210;
 	    	case 2:
 	    		return 194;
 	    	case 3:
-	    		return 172;
+	    		return 178;
 	    	case 4:
-	    		return 156;
+	    		return 162;
 	    	case 5:
-	    		return 140;
+	    		return 146;
 	    	case 6:
-	    		return 124;
+	    		return 130;
 	    	case 7:
-	    		return 108;
+	    		return 114;
 	    	default:
-	    		return this.blockIndexInTexture;
+	    		return 64;
         }
     }
-
-    /**
-     * Returns the block texture based on the side being looked at.  Args: side
-     */
+    
     public int getBlockTextureFromSide(int par1)
     {
         return this.getBlockTextureFromSideAndMetadata(par1, 0);
     }
-
-    /**
-     * Returns the ID of the items to drop on destruction.
-     */
+    
     public int idDropped(int par1, Random par2Random, int par3)
     {
-        return Block.woolSingleSlab1.blockID;
+        return Block.woodSingleSlab.blockID;
     }
-
-    /**
-     * Returns an item stack containing a single instance of the current block type. 'i' is the block's subtype/damage
-     * and is ignored for blocks which do not support subtypes. Blocks which cannot be harvested should return null.
-     */
+    
     protected ItemStack createStackedBlock(int par1)
     {
-        return new ItemStack(Block.woolSingleSlab1.blockID, 2, par1 & 7);
+        return new ItemStack(Block.woodSingleSlab.blockID, 2, par1 & 7);
     }
-
-    /**
-     * Returns the slab block name with step type.
-     */
+    
     public String getFullSlabName(int par1)
     {
-        if (par1 < 0 || par1 >= dyeColorNames.length)
+        if (par1 < 0 || par1 >= woolType.length)
         {
             par1 = 0;
         }
 
-        return super.getBlockName() + "." + dyeColorNames[par1];
+        return super.getBlockName() + "." + woolType[par1];
     }
-
-    /**
-     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
-     */
+    
     public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
     {
         if (par1 != Block.woolDoubleSlab1.blockID)
         {
             for (int var4 = 0; var4 < 8; ++var4)
             {
-                if (var4 != 2)
-                {
-                    par3List.add(new ItemStack(par1, 1, var4));
-                }
+                par3List.add(new ItemStack(par1, 1, var4));
             }
         }
     }
--- fr/nukkit/blocks/BlockWoolStep2.java
+++ fr/nukkit/blocks/BlockWoolStep2.java
@@ -11,77 +11,70 @@ import net.minecraft.src.Material;
 
 public class BlockWoolStep2 extends BlockHalfSlab
 {
-	public static final String[] dyeColorNames = new String[] { "white", "orange", "magenta","lightBlue", "yellow", "lime", "pink", "gray" };
+    public static final String[] woolType = new String[] { "silver", "cyan", "purple", "blue", "brown", "green", "red", "black" };
 
-	public BlockWoolStep2(int par1, boolean par2)
-	{
-		super(par1, par2, Material.rock);
-		this.setCreativeTab(CreativeTabs.tabBlock);
-	}
-
-	public int getBlockTextureFromSideAndMetadata(int par1, int par2)
-	{
-        int metadata = par2 & 7;
-        
-        switch(metadata)
+    public BlockWoolStep2(int par1, boolean par2)
+    {
+        super(par1, par2, Material.wood);
+        this.setCreativeTab(CreativeTabs.tabBlock);
+    }
+    
+    public int getBlockTextureFromSideAndMetadata(int par1, int par2)
+    {
+        switch (par2 & 7)
         {
-        	case 0:
-        		return 64;
-        	case 1:
-        		return 210;
-        	case 2:
-        		return 194;
-        	case 3:
-        		return 172;
-        	case 4:
-        		return 156;
-        	case 5:
-        		return 140;
-        	case 6:
-        		return 124;
-        	case 7:
-        		return 108;
-        	default:
-        		return this.blockIndexInTexture;
+	    	case 1:
+	    		return 209;
+	    	case 2:
+	    		return 193;
+	    	case 3:
+	    		return 177;
+	    	case 4:
+	    		return 161;
+	    	case 5:
+	    		return 145;
+	    	case 6:
+	    		return 129;
+	    	case 7:
+	    		return 113;
+	    	default:
+	    		return 225;
         }
-	}
+    }
+    
+    public int getBlockTextureFromSide(int par1)
+    {
+        return this.getBlockTextureFromSideAndMetadata(par1, 0);
+    }
+    
+    public int idDropped(int par1, Random par2Random, int par3)
+    {
+        return Block.woodSingleSlab.blockID;
+    }
+    
+    protected ItemStack createStackedBlock(int par1)
+    {
+        return new ItemStack(Block.woodSingleSlab.blockID, 2, par1 & 7);
+    }
+    
+    public String getFullSlabName(int par1)
+    {
+        if (par1 < 0 || par1 >= woolType.length)
+        {
+            par1 = 0;
+        }
 
-	public int getBlockTextureFromSide(int par1)
-	{
-		return this.getBlockTextureFromSideAndMetadata(par1, 0);
-	}
-
-	public int idDropped(int par1, Random par2Random, int par3)
-	{
-		return Block.woolSingleSlab1.blockID;
-	}
-
-	protected ItemStack createStackedBlock(int par1)
-	{
-		return new ItemStack(Block.woolSingleSlab1.blockID, 2, par1 & 7);
-	}
-
-	public String getFullSlabName(int par1)
-	{
-		if (par1 < 0 || par1 >= dyeColorNames.length)
-		{
-			par1 = 0;
-		}
-
-		return super.getBlockName() + "." + dyeColorNames[par1];
-	}
-
-	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
-	{
-		if (par1 != Block.woolDoubleSlab2.blockID)
-		{
-			for (int var4 = 0; var4 < 8; ++var4)
-			{
-				if (var4 != 2)
-				{
-					par3List.add(new ItemStack(par1, 1, var4));
-				}
-			}
-		}
-	}
+        return super.getBlockName() + "." + woolType[par1];
+    }
+    
+    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
+    {
+        if (par1 != Block.woolDoubleSlab2.blockID)
+        {
+            for (int var4 = 0; var4 < 8; ++var4)
+            {
+                par3List.add(new ItemStack(par1, 1, var4));
+            }
+        }
+    }
 }
