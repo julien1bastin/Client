0cc8f4883db6e8cff260e621358044430d09b20b
Mathieu Bour <dierka@live.fr>
2013-03-05 01:01:17 +0100

    Adapting class for beer items.

--- fr/nukkit/blocks/BlockMaltCrops.java
+++ fr/nukkit/blocks/BlockMaltCrops.java
@@ -24,19 +24,12 @@ public class BlockMaltCrops extends BlockFlower
         this.disableStats();
         this.setRequiresSelfNotify();
     }
-
-    /**
-     * Gets passed in the blockID of the block below and supposed to return true if its allowed to grow on the type of
-     * blockID passed in. Args: blockID
-     */
+    
     protected boolean canThisPlantGrowOnThisBlockID(int par1)
     {
         return par1 == Block.tilledField.blockID;
     }
-
-    /**
-     * Ticks the block if it's been scheduled
-     */
+    
     public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
     {
         super.updateTick(par1World, par2, par3, par4, par5Random);
@@ -57,20 +50,12 @@ public class BlockMaltCrops extends BlockFlower
             }
         }
     }
-
-    /**
-     * Apply bonemeal to the crops.
-     */
+    
     public void fertilize(World par1World, int par2, int par3, int par4)
     {
         par1World.setBlockMetadataWithNotify(par2, par3, par4, 7);
     }
-
-    /**
-     * Gets the growth rate for the crop. Setup to encourage rows by halving growth rate if there is diagonals, crops on
-     * different sides that aren't opposing, and by adding growth for every crop next to this one (and for crop below
-     * this one). Args: x, y, z
-     */
+    
     private float getGrowthRate(World par1World, int par2, int par3, int par4)
     {
         float var5 = 1.0F;
@@ -119,47 +104,38 @@ public class BlockMaltCrops extends BlockFlower
 
         return var5;
     }
-
-    /**
-     * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
-     */
+    
     public int getBlockTextureFromSideAndMetadata(int par1, int par2)
     {
-        if (par2 < 0)
+        if (par2 >= 0 && par2 < 5)
         {
-            par2 = 7;
+            return this.blockIndexInTexture + par2;
         }
-
-        return this.blockIndexInTexture + par2;
+        else if (par2 > 4 && par2 < 8)
+        {
+        	return this.blockIndexInTexture + 11 + par2;
+        }
+        else
+        {
+        	return 235;
+        }
     }
-
-    /**
-     * The type of render function that is called for this block
-     */
+    
     public int getRenderType()
     {
         return 6;
     }
-
-    /**
-     * Generate a seed ItemStack for this crop.
-     */
+    
     protected int getSeedItem()
     {
-        return Item.seeds.itemID;
+        return Item.seedsMalt.itemID;
     }
-
-    /**
-     * Generate a crop produce ItemStack for this crop.
-     */
+    
     protected int getCropItem()
     {
-        return Item.wheat.itemID;
+        return Item.malt.itemID;
     }
-
-    /**
-     * Drops the block items with a specified chance of dropping the specified items
-     */
+    
     public void dropBlockAsItemWithChance(World par1World, int par2, int par3, int par4, int par5, float par6, int par7)
     {
         super.dropBlockAsItemWithChance(par1World, par2, par3, par4, par5, par6, 0);
@@ -180,26 +156,17 @@ public class BlockMaltCrops extends BlockFlower
             }
         }
     }
-
-    /**
-     * Returns the ID of the items to drop on destruction.
-     */
+    
     public int idDropped(int par1, Random par2Random, int par3)
     {
         return par1 == 7 ? this.getCropItem() : this.getSeedItem();
     }
-
-    /**
-     * Returns the quantity of items to drop on block destruction.
-     */
+    
     public int quantityDropped(Random par1Random)
     {
         return 1;
     }
-
-    /**
-     * only called by clickMiddleMouseButton , and passed to inventory.setCurrentItem (along with isCreative)
-     */
+    
     public int idPicked(World par1World, int par2, int par3, int par4)
     {
         return this.getSeedItem();
